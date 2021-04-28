3ee3d2cb74c3524d68e086ba4bef3165246fdc89
sergei <example@mail.ru>
2021-04-28 21:02:39 +0300

    add http get/post

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
 import { Todo } from '../interfaces';
 
 @Component({
@@ -10,15 +11,40 @@ import { Todo } from '../interfaces';
 export class HttpComponent implements OnInit {
 
   public todos!: Todo[]
+  public todoTitle: string = '';
+  public loading: boolean = false;
 
-  constructor( 
+  constructor(
     private readonly http: HttpClient,
     ) { }
 
   ngOnInit(): void {
-    this.http.get<Todo[]>("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    /* this.downloadTodo(); */
+  }
+
+  public addTodo(): void {
+    if(!this.todoTitle.trim()){
+      return
+    }
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>("https://jsonplaceholder.typicode.com/todos", newTodo)
     .subscribe(response => {
-      this.todos = response
+      this.todos.push(response);
+      this.todoTitle = '';
+    });
+  };
+
+  public downloadTodo(): void {
+    this.loading = true;
+    this.http.get<Todo[]>("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    .pipe(delay(1500))
+    .subscribe(response => {
+      this.todos = response;
+      this.loading = false;
     })
   }
 
